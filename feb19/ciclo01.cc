face107c5392df8bb7d8837214670ab03bd60918
asoleal <jlealgom@unal.edu.co>
2022-02-19 08:04:35 -0500

    update

--- feb19/ciclo01.cc
+++ feb19/ciclo01.cc
@@ -6,4 +6,10 @@ int main(int argc, char **argv){
         f[i]=2*i;
         std::cout <<"f["<<i<<"]="<<f[i] <<std::endl;
     }
+    double y[5]={2.0, 3.0, 4.0, 5.0, 6.0};
+    double z[5];
+    for (int i=0;i<5;i++){
+        z[i]=f[i]+y[i];
+        std::cout <<"z["<<i<<"]="<<z[i]<<std::endl;
+    }
 }
