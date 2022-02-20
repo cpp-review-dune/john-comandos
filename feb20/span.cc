a961ea563426aa748612146c6c20aaf8be712f42
asoleal <jlealgom@unal.edu.co>
2022-02-20 08:22:45 -0500

    span

--- feb20/span.cc
+++ feb20/span.cc
@@ -1,12 +1,52 @@
+#include <iostream>
 #include <cassert>  // for assert(...)
 #include <span>
-int main(){
-    int a[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-    int b[] { 8, 7, 6 };
-    int* b_ptr = b;
-    auto a_span = std::span{a};
-    auto b_span = std::span{b_ptr, 3};   // or: std::span<int,3>{b_ptr}
-    assert(a_span.size() == 9);
-    assert(b_span.size() == 3);
-    return 0;
+#include <array>
+
+int main()
+{
+  int a[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+  int b[] { 8, 7, 6 };
+  
+  int* b_ptr = b;
+  //std::array<int, 5> array1 = { 1, 2, 3, 4, 5};
+  //int* c_ptr = std::array<int, 5>
+
+  for (const auto& e : b)
+  {
+    std::cout << e << std::endl;
+
+  }
+
+  for (int i = 0; i < 3; i++)
+  {
+    std::cout << b_ptr[i] << std::endl;
+  }
+
+  auto a_span = std::span{a};
+
+  std::cout << "------------------------" << std::endl;
+
+  for (const auto& e : a_span)
+  {
+    std::cout << e << std::endl;
+  }
+
+    std::cout << "------------------------" << std::endl;
+
+  auto b_span = std::span{b_ptr, 3};   // or: std::span<int,3>{b_ptr}
+
+  for (const auto& e : b_span)
+  {
+    std::cout << e << std::endl;
+
+  }
+
+  return 0;
 }
+
+
+/*
+assert(a_span.size() == 9);
+assert(b_span.size() == 3);
+*/
